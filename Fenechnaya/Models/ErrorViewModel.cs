03a9f86a37b62a02e856eccc6678b20222a8844e
Alexey <Alexey@DESKTOP-NISHU0E>
2024-08-19 15:46:12 +0300

    Добавьте файлы проекта.

---
+++ Fenechnaya/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Fenechnaya.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
